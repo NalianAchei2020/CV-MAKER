4df2aab13f31de1bc9e9cdbb44cad20aee68711f
chiwi2024 <nalianachei2@gmail.com>
2025-02-07 21:35:43 +0100

    Create header.tsx

---
+++ frontend/src/components/header/header.tsx
@@ -0,0 +1,24 @@
+const Header = () => {
+  return (
+    <>
+      <header className="flex flex-col">
+        <div>logo</div>
+        <div>
+          <ul>
+            <li>HOME</li>
+            <li>ABOUT US</li>
+            <li>PRICING</li>
+            <li>TEMPLATES</li>
+            <li>CONTACT</li>
+            <li>
+              <button>LOGIN IN</button>
+            </li>
+            <li>
+              <button>SIGNUP</button>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </>
+  );
+};
