2aad8f7a2ca5cb41311ec4cb1de75cf5c616e162
chiwi2024 <nalianachei2@gmail.com>
2025-02-09 07:09:26 +0100

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,20 @@
-import Image from 'next/image';
+import AboutUs from '@/components/home/about_us';
+import FeatureSection from '@/components/home/features';
+import HeroSection from '@/components/home/her0Section';
+
+import FeaturesGrid from '@/components/home/mainFeatures';
+import PricingSection from '@/components/home/pricing';
+import TemplateSection from '@/components/home/templateSection';
 
 export default function Home() {
-  return <div>CV maker</div>;
+  return (
+    <main className="min-h-screen">
+      <HeroSection />
+      <FeatureSection />
+      <AboutUs />
+      <TemplateSection />
+      <PricingSection />
+      <FeaturesGrid />
+    </main>
+  );
 }
